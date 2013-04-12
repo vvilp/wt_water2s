b3bbba6711153286aebe9357d2c3cbb2eec19066
vvilp <vvilp.wu@gmail.com>
2013-04-13 01:20:28 +0800

    + add wt_FluidLB

---
+++ wt_FluidLB.cpp
@@ -0,0 +1,8 @@
+//
+//  lattice boltzmann Method simulate the Flow 
+//
+//  Created by vvilp on 4/13/13.
+//  Copyright (c) 2013 vvilp. All rights reserved.
+//
+
+#include "wt_FluidLB.h"
---
+++ wt_FluidLB.h
@@ -0,0 +1,13 @@
+//
+//  lattice boltzmann Method simulate the Flow
+//
+//  Created by vvilp on 4/13/13.
+//  Copyright (c) 2013 vvilp. All rights reserved.
+//
+
+#ifndef __wt_water3__wt_FluidLB__
+#define __wt_water3__wt_FluidLB__
+
+#include <iostream>
+
+#endif /* defined(__wt_water3__wt_FluidLB__) */
