54417f3ad17fac6b8f63d129c43be33c190d5923
vvilp <vvilp.wu@gmail.com>
2013-04-15 11:48:46 +0800

    + add new wt_glapp.h

---
+++ wt_glapp.h
@@ -0,0 +1,25 @@
+//
+//  Wt_GLApp.h
+//  wt_water3
+//
+//  Created by vvilp on 4/12/13.
+//  Copyright (c) 2013 vvilp. All rights reserved.
+//
+
+#ifndef __wt_water3__Wt_GLApp__
+#define __wt_water3__Wt_GLApp__
+
+#include "wt_config.h"
+#include "wt_app_interface.h"
+class Wt_GLApp
+{
+public:
+    Wt_GLApp();
+    ~Wt_GLApp();
+
+    
+    void run();
+
+};
+
+#endif /* defined(__wt_water3__Wt_GLApp__) */
